54ffd0cb4de5879f183dfe16684b0d82d3b6b387
ajs7777 <abhisaha955@gmail.com>
2025-01-20 23:33:55 +0530

    Initial Commit

---
+++ Goodine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Goodine
+//
+//  Created by Abhijit Saha on 20/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Goodine/GoodineApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodineApp.swift
+//  Goodine
+//
+//  Created by Abhijit Saha on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct GoodineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
