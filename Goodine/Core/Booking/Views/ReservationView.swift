3d579a610c5ce08df538f870184b4ff3ea99c616
ajs7777 <abhisaha955@gmail.com>
2025-01-29 01:33:45 +0530

    [Feature] Add a view for reseravation details

---
+++ Goodine/Core/Booking/Views/ReservationView.swift
@@ -0,0 +1,197 @@
+//
+//  ReservationView.swift
+//  Goodine
+//
+//  Created by Abhijit Saha on 29/01/25.
+//
+
+import SwiftUI
+import MapKit
+
+struct ReservationView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    @State var cameraPosition : MapCameraPosition
+    @State var name = ""
+    @State var email = ""
+    @State var phoneNumber = ""
+    @State var showConfirSheet = false
+    
+    init() {
+        
+        let region = MKCoordinateRegion(
+            
+            center: CLLocationCoordinate2D(latitude: 23.831457, longitude: 91.286781), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self._cameraPosition = State(initialValue: .region(region))
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Your Reservation")
+                    .foregroundStyle(.mainbw)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                
+                Spacer()
+                
+                Button(action: {
+                    dismiss()
+                }) {
+                    Image(systemName: "xmark")
+                        .foregroundColor(.mainbw)
+                        .fontWeight(.bold)
+                        .padding(.trailing)
+                }
+            }
+            .padding(.top)
+            .padding(.vertical)
+            ScrollView {
+                Map(position: $cameraPosition)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 150)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                
+                VStack(alignment: .leading, spacing: 5.0){
+                    Text("Limelight - Royal Orchid Hotel")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    Text("Indiranagar, Bangalore, India")
+                        .font(.footnote)
+                        .foregroundStyle(.secondary)
+                    
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.vertical)
+                
+                Divider()
+                
+                VStack(alignment: .leading, spacing: 15.0){
+                    HStack(spacing: 10.0){
+                        Image(systemName: "person.2")
+                            .fontWeight(.semibold)
+                        Text("03 Guest")
+                            .font(.callout)
+                    }
+                    .fontWeight(.bold)
+                    
+                    HStack(spacing: 16.0){
+                        Image(systemName: "calendar")
+                            .fontWeight(.semibold)
+                        Text("Tuesday, 31th Jan | 07:00 PM")
+                            .font(.callout)
+                    }
+                    .fontWeight(.bold)
+                    
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.vertical)
+                
+                Divider()
+                
+                VStack(alignment: .leading, spacing: 8.0){
+                    Text("Name")
+                    TextField( "", text: $name)
+                        .padding(.leading)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 55)
+                        .background(
+                            RoundedRectangle(cornerRadius: 8)
+                                .inset(by: 3)
+                                .stroke(.gray, lineWidth: 1)
+                        )
+                    
+                }
+                .padding(.top, 12)
+                
+                VStack(alignment: .leading, spacing: 8.0){
+                    Text("Email")
+                    TextField( "", text: $email)
+                        .padding(.leading)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 55)
+                        .background(
+                            RoundedRectangle(cornerRadius: 8)
+                                .inset(by: 3)
+                                .stroke(.gray, lineWidth: 1)
+                        )
+                    
+                }
+                .padding(.top, 12)
+                
+                VStack(alignment: .leading, spacing: 8.0){
+                    Text("Phone Number")
+                    TextField( "", text: $phoneNumber)
+                        .padding(.leading, 85)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 55)
+                        .background(
+                            RoundedRectangle(cornerRadius: 8)
+                                .inset(by: 3)
+                                .stroke(.gray, lineWidth: 1)
+                        )
+                        .overlay(alignment: .leading) {
+                            HStack{
+                                Text("🇮🇳")
+                                    .font(.title)
+                                Image(systemName: "chevron.down")
+                                    .font(.caption)
+                            }
+                            .padding(.leading)
+                        }
+                    
+                }
+                .padding(.top, 12)
+                
+                
+                Button {
+                    
+                } label: {
+                    Text("Add instructions To restaurant")
+                        .font(.callout)
+                        .tint(.gray)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading)
+                        .frame(height: 50)
+                        .background(Color(.systemGray6))
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                }
+                .padding(.vertical, 30)
+                .padding(.bottom, 100)
+
+                
+                Spacer()
+                
+            
+            } .scrollIndicators(.hidden)
+        }
+        .padding(.horizontal)
+        .overlay(alignment: .bottom) {
+            ZStack {
+                Color.mainInvert.ignoresSafeArea()
+                    .frame(height: 100)
+                Button {
+                    showConfirSheet.toggle()
+                } label: {
+                    Text("Confirm Booking")
+                        .font(.headline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.mainInvert)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 60)
+                        .background(Color.mainbw)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                        .padding()
+                }
+            }
+        }
+        .sheet(isPresented: $showConfirSheet, content: {
+            Text("Booking Confirmed")
+        })
+
+    }
+}
+
+#Preview {
+    ReservationView()
+}
