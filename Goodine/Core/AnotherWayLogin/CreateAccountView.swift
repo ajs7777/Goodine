af3647ecef4b32339a92022875ceb17d06495ea6
ajs7777 <abhisaha955@gmail.com>
2025-01-31 22:34:31 +0530

    [Feature] add view from users can log in with email

---
+++ Goodine/Core/AnotherWayLogin/CreateAccountView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateAccountView.swift
+//  Goodine
+//
+//  Created by Abhijit Saha on 31/01/25.
+//
+
+import SwiftUI
+
+struct CreateAccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateAccountView()
+}
