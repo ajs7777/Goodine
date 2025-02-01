5394419193c26186803bd60949f599ade26b2329
ajs7777 <abhisaha955@gmail.com>
2025-02-01 08:40:34 +0530

    [Feature] add a view to create user accounts

--- Goodine/Core/AnotherWayLogin/CreateAccountView.swift
+++ Goodine/Core/AnotherWayLogin/CreateAccountView.swift
@@ -8,8 +8,109 @@
 import SwiftUI
 
 struct CreateAccountView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack{
+                //dimiss button
+                HStack{
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "xmark")
+                            .tint(.mainbw)
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                    }
+                    
+                    Spacer()
+                }
+                
+                HStack{
+                    Image(.goodinetext)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 120, height: 30)
+                        .padding(.leading, -4)
+                    
+                    Spacer()
+                }
+                .padding(.top, 40)
+                
+                VStack(alignment: .leading){
+                    Text("Get started on Goodine")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Text("Create an accaount to get the best dine in experice, Like never before")
+                        .font(.footnote)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                //name, email, password
+                VStack(spacing: 12.0){
+                    
+                    TextField("Name", text: $name)
+                        .padding(18)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .inset(by: 3)
+                                .stroke(style: StrokeStyle(lineWidth: 1)))
+                    
+                    TextField("Enter your email", text: $email)
+                        .padding(18)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .inset(by: 3)
+                                .stroke(style: StrokeStyle(lineWidth: 1)))
+                    
+                    TextField("Enter your password", text: $password)
+                        .padding(18)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .inset(by: 3)
+                                .stroke(style: StrokeStyle(lineWidth: 1)))
+                    
+                    NavigationLink {
+                        LoginWithEmail()
+                            .navigationBarBackButtonHidden()
+                    } label: {
+                        Text("Sign Up")
+                            .goodineButtonStyle(.mainbw)
+                    }
+                    .padding(.vertical)
+                    
+                }
+                .padding(.top, 30)
+                
+                Spacer()
+                VStack {
+                    Image(.goodinetext)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 50)
+                        .opacity(0.4)
+                    HStack(spacing: 0.0){
+                        Text("I accept the")
+                        Text(" Terms & Conditions ")
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.mainbw)
+                        Text(" & ")
+                        Text(" Privacy Policy")
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.mainbw)
+                    }
+                    .foregroundStyle(.gray)
+                    .font(.caption2)
+                }
+            }
+            .padding()
+        }
     }
 }
 
