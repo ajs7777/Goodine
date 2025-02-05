1c2d4487f317af4d7f5d8c498f26e23d84d138b6
ajs7777 <abhisaha955@gmail.com>
2025-02-05 23:20:56 +0530

    [Feature] add an imagepicker for selecting images

---
+++ Goodine/Core/RestairantsDetails/Views/RestaurantImagePicker.swift
@@ -0,0 +1,51 @@
+//
+//  RestaurantImagePicker.swift
+//  Goodine
+//
+//  Created by Abhijit Saha on 05/02/25.
+//
+
+import PhotosUI
+import SwiftUI
+
+struct RestaurantImagePicker: UIViewControllerRepresentable {
+    @Binding var selectedImages: [UIImage]
+    
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.selectionLimit = 10  // Allow up to 10 images
+        config.filter = .images
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        let parent: RestaurantImagePicker
+        
+        init(_ parent: RestaurantImagePicker) {
+            self.parent = parent
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+            
+            for result in results {
+                result.itemProvider.loadObject(ofClass: UIImage.self) { (image, error) in
+                    if let image = image as? UIImage {
+                        DispatchQueue.main.async {
+                            self.parent.selectedImages.append(image)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
