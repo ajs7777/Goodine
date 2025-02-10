138c7021b8addabad1c65857275ca016c7c6702f
ajs7777 <abhisaha955@gmail.com>
2025-02-10 09:43:55 +0530

    [Feature] update the restaurant profile view so that the view can show the image in tab view at the to of the page

--- Goodine/Core/BusinessCore/RestairantsDetails/Views/RestaurantProfile.swift
+++ Goodine/Core/BusinessCore/RestairantsDetails/Views/RestaurantProfile.swift
@@ -10,155 +10,159 @@ import Kingfisher
 
 struct RestaurantProfile: View {
     @Environment(\.dismiss) var dismiss
-    @EnvironmentObject var businessAuthMV : BusinessAuthViewModel
-//    @EnvironmentObject var restaurantVM : RestaurantsDetailsViewModel
+    @ObservedObject var businessAuthMV = BusinessAuthViewModel()
     @State var showEditProfile : Bool = false
     
     var body: some View {
         ScrollView {
-                if let restaurant = businessAuthMV.restaurant {
-                    VStack(alignment: .leading) {
-                        TabView {
-                            if restaurant.imageUrl.isEmpty {
-                                Image(systemName: "photo")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(height: 50)
-                                    .foregroundStyle(.gray.opacity(0.6))
+            if let imageUrls = businessAuthMV.restaurant?.imageUrls, !imageUrls.isEmpty {
+                TabView {
+                    ForEach(imageUrls, id: \.self) { imageUrl in
+                        AsyncImage(url: URL(string: imageUrl)) { phase in
+                            if let image = phase.image {
+                                image.resizable()
+                                    .scaledToFill()
+                                    .frame(height: 250)
+                                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                                    .padding(.horizontal)
                             } else {
-                                KFImage(URL(string: restaurant.imageUrl))
-                                        .resizable()
-                                        .scaledToFill()
-                                        .frame(height: 250)
-                                        .clipped()
-                                
+                                ProgressView()
+                                    .frame(height: 250)
                             }
                         }
-                        .frame(height: 250)
-                        .tabViewStyle(PageTabViewStyle())
-                        VStack(alignment: .leading, spacing: 8) {
-                            HStack {
-                                Text("Open Now")
-                                    .foregroundStyle(.black.opacity(0.8))
-                                    .font(.callout)
-                                    .fontWeight(.semibold)
-                                    .padding(7)
-                                    .background(.openGreen)
-                                    .clipShape(RoundedRectangle(cornerRadius: 8))
-                                
-                                Spacer()
-                                Button {
-                                    showEditProfile.toggle()
-                                } label: {
-                                    Text("Edit Profile")
-                                        .font(.subheadline)
-                                        .foregroundStyle(.mainbw)
-                                }
-                                .buttonStyle(BorderedButtonStyle())
-                            }
-                            
-                            Text(restaurant.name)
-                                .foregroundStyle(.mainbw)
-                                .font(.largeTitle)
-                                .fontWeight(.bold)
-                            
-                            HStack {
-                                VStack(alignment: .leading) {
-                                    Text(restaurant.type)
-                                        .foregroundStyle(.mainbw)
-                                    Text("\(restaurant.city) | 2 Km")
-                                        .foregroundStyle(.secondary)
-                                }
-                                
-                                Spacer()
-                                
-                                VStack(alignment: .trailing, spacing: 8.0) {
-                                    HStack(alignment: .center, spacing: 3.0) {
-                                        Image(systemName: "star.fill")
-                                            .foregroundStyle(.yellow)
-                                            .font(.footnote)
-                                        Text("4.5(3k Ratings)")
-                                            .foregroundStyle(.mainbw)
-                                    }
-                                    .font(.callout)
-                                    
-                                    Text("₹\(restaurant.averageCost) for two")
-                                        .foregroundStyle(.mainbw)
-                                        .font(.callout)
-                                }
-                            }
-                            .padding(.bottom)
-                            
-                            Divider()
-                        }
-                        .padding(.top, 8)
-                        .padding(.horizontal)
-                        
-                        VStack(alignment: .leading, spacing: 20.0) {
-                            Text("Location")
-                                .foregroundStyle(.mainbw)
-                                .font(.title)
-                                .fontWeight(.bold)
-                            
-                            HStack(alignment: .top, spacing: 20.0) {
-                                Image(systemName: "location.circle")
-                                    .foregroundStyle(.mainbw)
-                                    .font(.title2)
-                                    .fontWeight(.medium)
-                                Text(restaurant.address)
-                                    .foregroundStyle(.mainbw)
-                                    .font(.subheadline)
-                                Spacer()
-                            }
-                            
-                            HStack(alignment: .top, spacing: 20.0) {
-                                Image(systemName: "clock")
-                                    .foregroundStyle(.mainbw)
-                                    .font(.title2)
-                                    .fontWeight(.medium)
-                                
-                                Text("\(restaurant.openingTime.formattedTime()) - \(restaurant.closingTime.formattedTime())")
-                                    .foregroundStyle(.mainbw)
-                                
-                                Spacer()
-                            }
-                            
-                            Divider()
-                        }
-                        .padding()
-                        
-                        VStack(alignment: .leading, spacing: 20.0) {
-                            Text("Features")
-                                .foregroundStyle(.mainbw)
-                                .font(.title)
-                                .fontWeight(.bold)
-                            
-                            HStack {
-                                Text("Reservation Available")
-                                    .foregroundStyle(.mainbw)
-                                    .fontWeight(.medium)
-                                    .padding(10)
-                                    .background(Color(.systemGray5))
-                                    .clipShape(RoundedRectangle(cornerRadius: 8))
-                                Text("Parking Available")
-                                    .foregroundStyle(.mainbw)
-                                    .fontWeight(.medium)
-                                    .padding(10)
-                                    .background(Color(.systemGray5))
-                                    .clipShape(RoundedRectangle(cornerRadius: 8))
-                                
-                                Spacer()
-                            }
-                        }
-                        .padding(.horizontal)
-                        .padding(.bottom, 30)
-                    }
-                } else {
-                    ProgressView()
-                        .onAppear {
-                        Task { await businessAuthMV.fetchUserDetails() }
                     }
                 }
+                .frame(height: 250)
+                .tabViewStyle(PageTabViewStyle()) // Adds swipe dots at the bottom
+                .indexViewStyle(PageIndexViewStyle())
+                .padding()
+            } else {
+                Image(systemName: "photo")
+                    .frame(height: 250)
+            }
+            if let restaurant = businessAuthMV.restaurant {
+                VStack(alignment: .leading) {
+                    VStack(alignment: .leading, spacing: 8) {
+                        HStack {
+                            Text("Open Now")
+                                .foregroundStyle(.black.opacity(0.8))
+                                .font(.callout)
+                                .fontWeight(.semibold)
+                                .padding(7)
+                                .background(.openGreen)
+                                .clipShape(RoundedRectangle(cornerRadius: 8))
+                            
+                            Spacer()
+                            Button {
+                                showEditProfile.toggle()
+                            } label: {
+                                Text("Edit Profile")
+                                    .font(.subheadline)
+                                    .foregroundStyle(.mainbw)
+                            }
+                            .buttonStyle(BorderedButtonStyle())
+                        }
+                        
+                        Text(restaurant.name)
+                            .foregroundStyle(.mainbw)
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(restaurant.type)
+                                    .foregroundStyle(.mainbw)
+                                Text("\(restaurant.city) | 2 Km")
+                                    .foregroundStyle(.secondary)
+                            }
+                            
+                            Spacer()
+                            
+                            VStack(alignment: .trailing, spacing: 8.0) {
+                                HStack(alignment: .center, spacing: 3.0) {
+                                    Image(systemName: "star.fill")
+                                        .foregroundStyle(.yellow)
+                                        .font(.footnote)
+                                    Text("4.5(3k Ratings)")
+                                        .foregroundStyle(.mainbw)
+                                }
+                                .font(.callout)
+                                
+                                Text("₹\(restaurant.averageCost) for two")
+                                    .foregroundStyle(.mainbw)
+                                    .font(.callout)
+                            }
+                        }
+                        .padding(.bottom)
+                        
+                        Divider()
+                    }
+                    .padding(.top, 8)
+                    .padding(.horizontal)
+                    
+                    VStack(alignment: .leading, spacing: 20.0) {
+                        Text("Location")
+                            .foregroundStyle(.mainbw)
+                            .font(.title)
+                            .fontWeight(.bold)
+                        
+                        HStack(alignment: .top, spacing: 20.0) {
+                            Image(systemName: "location.circle")
+                                .foregroundStyle(.mainbw)
+                                .font(.title2)
+                                .fontWeight(.medium)
+                            Text(restaurant.address)
+                                .foregroundStyle(.mainbw)
+                                .font(.subheadline)
+                            Spacer()
+                        }
+                        
+                        HStack(alignment: .top, spacing: 20.0) {
+                            Image(systemName: "clock")
+                                .foregroundStyle(.mainbw)
+                                .font(.title2)
+                                .fontWeight(.medium)
+                            
+                            Text("\(restaurant.openingTime.formattedTime()) - \(restaurant.closingTime.formattedTime())")
+                                .foregroundStyle(.mainbw)
+                            
+                            Spacer()
+                        }
+                        
+                        Divider()
+                    }
+                    .padding()
+                    
+                    VStack(alignment: .leading, spacing: 20.0) {
+                        Text("Features")
+                            .foregroundStyle(.mainbw)
+                            .font(.title)
+                            .fontWeight(.bold)
+                        
+                        HStack {
+                            Text("Reservation Available")
+                                .foregroundStyle(.mainbw)
+                                .fontWeight(.medium)
+                                .padding(10)
+                                .background(Color(.systemGray5))
+                                .clipShape(RoundedRectangle(cornerRadius: 8))
+                            Text("Parking Available")
+                                .foregroundStyle(.mainbw)
+                                .fontWeight(.medium)
+                                .padding(10)
+                                .background(Color(.systemGray5))
+                                .clipShape(RoundedRectangle(cornerRadius: 8))
+                            
+                            Spacer()
+                        }
+                    }
+                    .padding(.horizontal)
+                    .padding(.bottom, 30)
+                }
+            } else {
+                ProgressView()
+                    
+            }
             Button{
                 Task{
                     try businessAuthMV.signOut()
@@ -169,9 +173,12 @@ struct RestaurantProfile: View {
             }
             .padding(.bottom, 120)
         }
+//        .onAppear {
+//            Task { await businessAuthMV.fetchUserDetails() }
+//        }
         .ignoresSafeArea()
         .sheet(isPresented: $showEditProfile) {
-            RestaurantsDetailsForm()
+            RestaurantsDetailsForm(businessAuthMV: businessAuthMV)
         }
     }
 }
@@ -180,31 +187,6 @@ struct RestaurantProfile: View {
     RestaurantProfile()
 }
 
-//extension RestaurantProfile {
-    
-//    private func restaurantImages(_ imageURLs: [String]) -> some View {
-//        TabView {
-//            if imageURLs.isEmpty {
-//                Image(systemName: "photo")
-//                    .resizable()
-//                    .scaledToFit()
-//                    .frame(height: 50)
-//                    .foregroundStyle(.gray.opacity(0.6))
-//            } else {
-//                ForEach(imageURLs, id: \.self) { imageUrl in
-//                    KFImage(URL(string: imageUrl))
-//                        .resizable()
-//                        .scaledToFill()
-//                        .frame(height: 250)
-//                        .clipped()
-//                }
-//            }
-//        }
-//        .frame(height: 250)
-//        .tabViewStyle(PageTabViewStyle())
-//    }
-
-//}
 
 extension Date {
     func formattedTime() -> String {
