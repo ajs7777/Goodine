d112b37ddf0baf1a239dc7bcd7e3c7e68ecba652
ajs7777 <abhisaha955@gmail.com>
2025-02-20 23:26:45 +0530

    [Clena] update some changes

---
+++ Goodine/Core/BusinessCore/RstaurantMenu/Views/RestaurantMenu.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantMenu.swift
+//  Goodine
+//
+//  Created by Abhijit Saha on 19/02/25.
+//
+
+import SwiftUI
+
+struct RestaurantMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RestaurantMenu()
+}
---
+++ Goodine/Models/MenuItem.swift
@@ -0,0 +1,8 @@
+//
+//  MenuItem.swift
+//  Goodine
+//
+//  Created by Abhijit Saha on 19/02/25.
+//
+
+import Foundation
