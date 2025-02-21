ecb2bdaf49cd913ad0bee0aa5d61ac776988c714
ajs7777 <abhisaha955@gmail.com>
2025-02-21 20:33:41 +0530

    [Feature] add a model for menu items

--- Goodine/Models/MenuItem.swift
+++ Goodine/Models/MenuItem.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct MenuItem : Identifiable, Codable {
+    let id: String
+    let foodname: String
+    let foodDescription: String?
+    let foodPrice: Int
+    let foodQuantity: Int
+    let foodImage: String?
+    var veg : Bool = false
+}
+
+
+extension MenuItem {
+    static var MockData : [MenuItem] = [
+        .init(id: "1", foodname: "Veg Burger", foodDescription: "A juicy burger made with fresh vegetables", foodPrice: 100, foodQuantity: 1, foodImage: nil),
+        .init(id: "2", foodname: "Non Veg Burger", foodDescription: "A juicy burger made with fresh vegetables", foodPrice: 150, foodQuantity: 1, foodImage: nil),
+    ]
+}
