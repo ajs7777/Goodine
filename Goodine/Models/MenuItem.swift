5220eaa071c43da67ed8c60a8b9f90bef73ce481
ajs7777 <abhisaha955@gmail.com>
2025-02-22 20:18:12 +0530

    [Clean] update some changes in the model

--- Goodine/Models/MenuItem.swift
+++ Goodine/Models/MenuItem.swift
@@ -12,15 +12,8 @@ struct MenuItem : Identifiable, Codable {
     let foodname: String
     let foodDescription: String?
     let foodPrice: Int
-    let foodQuantity: Int
-    let foodImage: String?
+    let foodQuantity: Int?
+    var foodImage: String?
     var veg : Bool = false
 }
 
-
-extension MenuItem {
-    static var MockData : [MenuItem] = [
-        .init(id: "1", foodname: "Veg Burger", foodDescription: "A juicy burger made with fresh vegetables", foodPrice: 100, foodQuantity: 1, foodImage: nil),
-        .init(id: "2", foodname: "Non Veg Burger", foodDescription: "A juicy burger made with fresh vegetables", foodPrice: 150, foodQuantity: 1, foodImage: nil),
-    ]
-}
