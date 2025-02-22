fe61e3a76b83056cae1f36acaf04ce9dfa69ae0e
ajs7777 <abhisaha955@gmail.com>
2025-02-22 20:19:58 +0530

    [Feature] use kingfisher for image caching and populating the model data so the view can work dynamically

--- Goodine/Core/BusinessCore/RstaurantMenu/Views/FoodRowView.swift
+++ Goodine/Core/BusinessCore/RstaurantMenu/Views/FoodRowView.swift
@@ -6,81 +6,103 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct FoodRowView: View {
+    let menuItem: MenuItem
+    var onDelete: () -> Void
     
-    @State var count: Int = 0
-    let menuItem : [MenuItem]
     
     var body: some View {
-        HStack{
-            Image(menuItem[0].foodImage ?? "fork.knife")
-                .resizable()
-                .scaledToFill()
-                .frame(width: 80, height: 80)
-                .clipShape(RoundedRectangle(cornerRadius: 12))
-                .overlay(alignment: .bottomLeading) {
-                    VegNonVegIcon(size: 15, color: .red)
-                        .padding(8)
-                }
+        HStack(spacing: 10){
+            if let imageUrl = menuItem.foodImage, let url = URL(string: imageUrl) {
+                           KFImage(url)
+                               .resizable()
+                               .placeholder {
+                                           ProgressView()
+                                       }
+                               .scaledToFill()
+                               .frame(width: 80, height: 80)
+                               .clipShape(RoundedRectangle(cornerRadius: 12))
+                               .overlay(alignment: .bottomLeading) {
+                                   VegNonVegIcon(size: 15, color: menuItem.veg ? .green : .red)
+                                       .padding(8)
+                               }
+                               
+                       } else {
+                           Image(systemName: "photo")
+                               .resizable()
+                               .scaledToFit()
+                               .foregroundColor(.mainbw.opacity(0.5))
+                               .frame(width: 30, height: 30)
+                               .padding(25)
+                               .background(.mainbw.opacity(0.2))
+                               .clipShape(RoundedRectangle(cornerRadius: 12))
+                               .overlay(alignment: .bottomLeading) {
+                                   VegNonVegIcon(size: 15, color: menuItem.veg ? .green : .red)
+                                       .padding(8)
+                               }
+                       }
+                
             
             VStack(alignment: .leading){
-                Text(menuItem[0].foodname)
+                Text(menuItem.foodname)
                     .fontWeight(.bold)
-                Text(menuItem[0].foodDescription ?? "Food Description")
+                Text(menuItem.foodDescription ?? "Food Description")
                     .font(.caption)
                     .foregroundStyle(.mainbw.opacity(0.5))
                 
-                Text("₹\(menuItem[0].foodPrice)")
+                Text("₹\(menuItem.foodPrice)")
                     .foregroundStyle(.mainbw.opacity(0.5))
             }
             
             Spacer()
             
             VStack{
-                Button{
-                    
-                }label: {
+                Button(action: onDelete) {
                     Image(systemName: "trash")
+                        .font(.system(size: 20, weight: .bold))
                         .foregroundStyle(.red)
                 }
             }
-            
-//            HStack(spacing: 8) {
-//                        Button(action: {
-//                            if count > 0 { count -= 1 } // Prevent going below 0
-//                        }) {
-//                            Image(systemName: "minus")
-//                                .foregroundStyle(.mainbw)
-//                                .font(.system(size: 15, weight: .heavy))
-//                                .frame(width: 20, height: 15)
-//                                .background(Color(.systemGray6))
-//                                .clipShape(Circle())
-//                        }
-//                        .disabled(count == 0) // Disable button at 0
-//                        .opacity(count == 0 ? 0.5 : 1.0) // Reduce opacity when disabled
-//
-//                        Text("\(count)")
-//                            .font(.system(size: 15, weight: .heavy))
-//                            .frame(width: 20, alignment: .center)
-//
-//                        Button(action: { count += 1 }) {
-//                            Image(systemName: "plus")
-//                                .foregroundStyle(.mainbw)
-//                                .font(.system(size: 15, weight: .heavy))
-//                                .frame(width: 20, height: 15)
-//                                .background(Color(.systemGray6))
-//                                .clipShape(Circle())
-//                        }
-//                    }
-//                    .padding(8)
-//                    .background(Color(.systemGray6))
-//                    .clipShape(RoundedRectangle(cornerRadius: 12))
-//                    .shadow(radius: 1)
         }
+        
     }
 }
 
-#Preview {
-    FoodRowView( menuItem: [MenuItem(id: "1", foodname: "Mutton Biriyani", foodDescription: "Best Biriyani", foodPrice: 599, foodQuantity: 1, foodImage: "biriyani")])
-}
+//#Preview {
+//    FoodRowView( menuItem: [MenuItem(id: "1", foodname: "Mutton Biriyani", foodDescription: "Best Biriyani", foodPrice: 599, foodQuantity: 1, foodImage: "fork.knife")])
+//}
+
+
+//HStack(spacing: 8) {
+//            Button(action: {
+//                if count > 0 { count -= 1 } // Prevent going below 0
+//            }) {
+//                Image(systemName: "minus")
+//                    .foregroundStyle(.mainbw)
+//                    .font(.system(size: 15, weight: .heavy))
+//                    .frame(width: 20, height: 15)
+//                    .background(Color(.systemGray6))
+//                    .clipShape(Circle())
+//            }
+//            .disabled(count == 0) // Disable button at 0
+//            .opacity(count == 0 ? 0.5 : 1.0) // Reduce opacity when disabled
+//
+//            Text("\(count)")
+//                .font(.system(size: 15, weight: .heavy))
+//                .frame(width: 20, alignment: .center)
+//
+//            Button(action: { count += 1 }) {
+//                Image(systemName: "plus")
+//                    .foregroundStyle(.mainbw)
+//                    .font(.system(size: 15, weight: .heavy))
+//                    .frame(width: 20, height: 15)
+//                    .background(Color(.systemGray6))
+//                    .clipShape(Circle())
+//            }
+//        }
+//        .padding(8)
+//        .background(Color(.systemGray6))
+//        .clipShape(RoundedRectangle(cornerRadius: 12))
+//        .shadow(radius: 1)
